fba46e7b7218d94d9aedb0d0095ed7922aa271eb
manuans <manuans0756@gmail.com>
2026-07-24 17:31:50 +0530

    Day 9: Added arithmetic operator programs
    Added user input, arithmetic operations, increment, and decrement operator programs.

---
+++ Decrement_ Operator/Decrement_Operator/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+
+    printf("Before Decrement = %d\n", a);
+
+    a--;
+
+    printf("After Decrement = %d\n", a);
+
+    return 0;
+}
---
+++ Increment_Operator/Increment_Operator/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+
+    printf("Before Increment = %d\n", a);
+
+    a++;
+
+    printf("After Increment = %d\n", a);
+
+    return 0;
+}
