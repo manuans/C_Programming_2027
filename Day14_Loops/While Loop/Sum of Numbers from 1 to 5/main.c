75b113d5c8a94354e4d22fb9fc49b5e1e74a6de5
manuans <manuans0756@gmail.com>
2026-08-01 17:32:42 +0530

    Completed Day 14 while loop programs
    Added remaining while loop programs including sum of numbers and user input examples.

---
+++ Day14_Loops/While Loop/Sum of Numbers from 1 to 5/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 1;
+    int sum = 0;
+
+    while(i <= 5)
+    {
+        sum = sum + i;
+        i++;
+    }
+
+    printf("sum = %d", sum);
+
+    return 0;
+}
