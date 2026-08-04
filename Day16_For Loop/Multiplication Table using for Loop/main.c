81564497f20e497a7c601f521039ba4f360a648c
manuans <manuans0756@gmail.com>
2026-08-04 14:56:56 +0530

    Learned for loop in C
    Added for loop programs including counting, reverse counting, even numbers, multiplication table, and sum examples .

---
+++ Day16_For Loop/Multiplication Table using for Loop/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+
+    for(i = 1; i <= 10; i++)
+    {
+        printf("5 * %d = %d\n", i, 5 * i);
+    }
+
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
